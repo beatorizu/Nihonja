360f4c76f022ad52a7477732ce34990121c2de1e
bea <beatorizu>
2016-08-15 22:49:33 -0300

    Updated python module [models]
    Adding AudioField to card class.

--- Nihonja/cards/models.py
+++ Nihonja/cards/models.py
@@ -1,11 +1,14 @@
+from audiofield.fields import AudioField
 from django.db import models
 
 class Card(models.Model):
 
     phrase = models.CharField('Frase', max_length=100)
     slug = models.SlugField('Atalho')
-    kanji = models.CharField('Kanji')
-    image = models.ImageField(upload_to='cards/images', verbose_name='Image')
+    kanji = models.TextField('Kanji')
+    audio = AudioField(upload_to='cards/audios',
+                        ext_whitelist=('.mp3', '.wav', '.ogg'),
+                        help_text=('Allowed type - .mp3, .wav, .ogg'))
 
     created_at = models.DateTimeField('Created at', auto_now_add=True)
     updated_at = models.DateTimeField('Updated at', auto_now=True)
