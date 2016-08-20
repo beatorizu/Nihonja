4f4ff2466400c859167e6f80f7cfac89f74e0d19
bea <beatorizu>
2016-08-20 15:53:10 -0300

    Configure auto slug
    Added prepopulated_fields to auto complete slug field.

--- Nihonja/cards/admin.py
+++ Nihonja/cards/admin.py
@@ -5,5 +5,6 @@ class CardAdmin(admin.ModelAdmin):
 
     list_display = ['phrase', 'created_at', 'updated_at']
     search_fields = ['phrase', 'kanji', 'slug']
+    prepopulated_fields = {'slug': ('kanji',)}
 
 admin.site.register(Card, CardAdmin)
