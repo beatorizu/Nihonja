64aba282f85774869e983769f481f26296c3b8f8
bea <beatorizu>
2016-08-20 18:04:57 -0300

    Add Card class
    Implemented a real index and function to render individual card view.

--- Nihonja/cards/views.py
+++ Nihonja/cards/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Card
 
 def index(request):
+    cards = Card.objects.all()
     template_name = 'cards/index.html'
-    return render(request, template_name)
+    context = {
+        'cards': cards
+    }
+    return render(request, template_name, context)
+
+def review(request, pk):
+    card = Card.objects.get(pk=pk)
+    template_name = 'cards/review.html'
+    context = {
+        'card': card
+    }
+    return render(request, template_name, context)
