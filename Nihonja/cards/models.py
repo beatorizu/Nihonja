cd787e26cedc293ce10a540ec84d7c4a56c9ed2e
bea <beatorizu>
2016-08-20 13:26:41 -0300

    Add Custom Manager
    Added CardManager class to DRY code.

--- Nihonja/cards/models.py
+++ Nihonja/cards/models.py
@@ -1,6 +1,14 @@
 from audiofield.fields import AudioField
 from django.db import models
 
+class CardManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(phrase__icontains=query) |
+            models.Q(kanji__icontains=query)
+        )
+
 class Card(models.Model):
 
     phrase = models.CharField('Frase', max_length=100)
@@ -12,3 +20,5 @@ class Card(models.Model):
 
     created_at = models.DateTimeField('Created at', auto_now_add=True)
     updated_at = models.DateTimeField('Updated at', auto_now=True)
+
+    objects = CardManager()
