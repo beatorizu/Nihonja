5bae59fe78b59bf145402b59b118581bc305709c
bea <beatorizu>
2016-08-20 16:53:19 -0300

    Add cards urls and namespace

--- Nihonja/urls.py
+++ Nihonja/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('Nihonja.core.urls', namespace='core')),
+    url(r'^cards/', include('Nihonja.cards.urls', namespace='cards')),
     url(r'^admin/', admin.site.urls),
 ]
