2d1b647a9162c87618d749d089370aad03b77c5f
bea <beatorizu>
2016-08-20 18:05:45 -0300

    Add card view url

--- Nihonja/cards/urls.py
+++ Nihonja/cards/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = [
     url(r'^$', 'Nihonja.cards.views.index', name='index'),
+    url(r'^(?P<pk>\d+)/$', 'Nihonja.cards.views.review', name='review'),
 ]
