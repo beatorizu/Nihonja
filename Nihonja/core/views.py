538514e25c9fea14414e9d47ee29995eb41e040c
beatorizu <betrizaf@gmail.com>
2016-12-05 01:05:03 -0200

    Move contact form to contact template

--- Nihonja/core/views.py
+++ Nihonja/core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactCard
 
 def home(request):
     return render(request, 'home.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    context = {}
+    if request.method == 'POST':
+        form = ContactCard(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form.send_mail(card)
+            form = ContactCard()
+    else:
+        form = ContactCard()
+    template_name = 'contact.html'
+    context['form'] = form
+    return render(request, template_name, context)
